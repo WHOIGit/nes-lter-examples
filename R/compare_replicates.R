4cdafc604360669e5e9607e820d09ed7b5c16516
Joe Futrelle <joefutrelle@gmail.com>
2018-11-01 09:40:02 -0400

    adding R example

---
+++ R/compare_replicates.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+BASE.URL <- 'https://raw.githubusercontent.com/WHOIGit/nes-lter-examples/master/data/'
+
+csv.url <- paste(BASE.URL, 'neslter_sosik_chl.csv', sep='')
+
+# read the data from GitHub
+df <- read.csv(csv.url)
+
+# now select just EN608 WSW replicates
+df <- df %>%
+  filter(cruise == 'EN608') %>%
+  filter(filter_mesh_size == '>0')
+
+# split each replicate into its own dataframe
+rep.a = df %>% filter(replicate == 'a')
+rep.b = df %>% filter(replicate == 'b')
+
+# now convert to wide form for plotting purposes
+merged <- merge(rep.a, rep.b, by=c('cruise','cast','niskin'))
+
+# make a scatter plot of replicate a against replicate b
+ggplot(merged, aes(x=chl.x, y=chl.y)) + geom_point() +
+  xlab('Replicate A') +
+  ylab('Replicate B')
